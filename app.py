d0775e0097d84713bab77d199d9142c6a23fcb9c
Cloghost23 <160161093+Cloghost23@users.noreply.github.com>
2026-06-12 14:42:58 +0000

    Enhance video downloader with temporary storage and UI
    Refactor video download functionality to use a temporary directory and provide a download button for the downloaded file.

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
-import yt_dlp
 import streamlit as st
+import yt_dlp
+import os
+import tempfile
 
-# Define resolution and other options
-ydl_opts = {
-    'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
-    'outtmpl': '%(title)s.%(ext)s',
-    'noplaylist': True
-}
+st.title("Vid downloader")
+url = st.text_input("Enter the URL")
 
-def download_video(url):
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-    return info['title']
-
-# Streamlit UI
-url = st.text_input("Enter YouTube URL")
 if st.button("Download"):
     if url:
-        st.success(f"Downloaded: {download_video(url)}")   
+        with tempfile.TemporaryDirectory() as tmpdir:
+            ydl_opts = {
+                'outtmpl': os.path.join(tmpdir, '%(title)s.%(ext)s'),
+                'format': 'bestvideo + bestaudio',
+                'noplaylist': True
+            }
+
+            
+            try:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([url])
+
+                files = [f for f in os.listdir(tmpdir) if f.endswith(('.mp4', '.mp3', '.webm'))]
+                if files:
+                    files_path = os.path.join(tmpdir, files[0])
+
+
+                    with open(files_path, 'rb') as f:
+                        st.download_button(
+                            label="Download Files",
+                            data = f,
+                            file_name = files[0],
+                            mime='video/mp3'
+                        )
+                    st.success("Download ready")
+                else:
+                    st.error("No files found")
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+    else:
+        st.warning("Enter a valid URL")
